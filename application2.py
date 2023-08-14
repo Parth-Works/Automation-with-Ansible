05219627a75afa788ac7dbec66b0ca174f357b79
Parth_Works <70955588+Parth-Works@users.noreply.github.com>
2023-08-14 13:59:43 +0200

    UpdateD

--- application2.py
+++ application2.py
@@ -1,6 +1,7 @@
 import flask
 import time
 import socket
+import subprocess
 
 h_name = socket.gethostname()
 IP_address = socket.gethostbyname(h_name)
@@ -9,8 +10,15 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    Time= time.strftime("%H:%M:%S")
+    Time = time.strftime("%H:%M:%S")
     client_port = str(flask.request.environ.get('REMOTE_PORT'))
     hostname = h_name
     host_IP = IP_address
-    return Time+" This Service is from the Host: "+hostname+" ("+host_IP+") :" +client_port+".\n"
+
+    # Retrieve server uptime using the uptime command
+    uptime_output = subprocess.check_output(['uptime']).decode('utf-8')
+    uptime_info = uptime_output.split(',')[0].strip()
+
+    return (Time + " This Service is from the Host: " + hostname +
+            " (" + host_IP + "):" + client_port + ".\n" +
+            "Server Uptime: " + uptime_info + ".\n")
